6d464bd485d73f5793ab0e4c941a0c8cde70712c
santos dave <santosdave86@gmail.com>
2021-08-15 16:30:23 +0300

    update

--- components/DocumentRow.js
+++ components/DocumentRow.js
@@ -1,8 +1,8 @@
 import Button from '@material-tailwind/react/Button';
 import Icon from '@material-tailwind/react/Icon';
-
+import {useRouter} from "next/dist/client/router";
 function DocumentRow({id,fileName,date}) {
-    
+    const router= useRouter();
     return (
         <div className="flex items-center p-4 rounded-lg hover:bg-gray-100 text-gray-700 text-sm cursor-pointer">
             <Icon name="article" size="3xl" color="blue"/>
